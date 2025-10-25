667183527d7a1a1b82d78511579e21d3e1ea89b5
Joshua Nwajei <nwajeijoshua55@gmail.com>
2025-10-25 18:06:46 +0100

    glow

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,103 +1,167 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import Timer from './Timer';
 import Side from './Side';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Hero() {
-    return (
-        <div className='overall'>
-            
-            <div className="containe">
+  return (
+    <div className="overall">
+      <div className="containe">
 
-                {/* ✅ Slideshow added here */}
-                 {/* <Slideshow /> */}
+        {/* 🔹 Floating main image animation */}
+        <motion.div
+          className="framer72"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, ease: 'easeOut' }}
+        >
+          <img src="/images/codmbout.png" alt="" />
+        </motion.div>
 
-                <div className="framer72">
-                  
-                    <img src="/images/codmbout.png" alt="" />
-                </div>
+        {/* 🔹 Glow frame fade-in */}
+        <motion.div
+          className="framer21"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1.5, ease: 'easeOut', delay: 0.3 }}
+        >
+          <img src="/images/framer.png" alt="" />
+        </motion.div>
 
-                <div className="framer21">  
-                    <img src="/images/framer.png" alt="" />
-                </div>
+        {/* 🔹 VS Battle Section */}
+        <motion.div
+          className="vs-container"
+          initial={{ scale: 0.5, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1.2, delay: 0.6 }}
+        >
+          <div className="xg-contain">
+            <motion.img
+              className="imgs"
+              src="/images/wlogo.png"
+              alt=""
+              animate={{ scale: [1, 1.1, 1], filter: ['drop-shadow(0 0 0px #00ff88)', 'drop-shadow(0 0 10px #00ff88)', 'drop-shadow(0 0 0px #00ff88)'] }}
+              transition={{ repeat: Infinity, duration: 2 }}
+            />
+            <span className="texts">XG</span>
+          </div>
 
-                <div className="vs-container">
-                    <div className="xg-contain">
-                        <img className='imgs' src="/images/wlogo.png" alt="" />
-                        <span className='texts'>XG</span>
-                    </div>
+          <motion.div
+            className="versus"
+            animate={{ scale: [1, 1.2, 1], rotate: [0, 10, -10, 0] }}
+            transition={{ repeat: Infinity, duration: 3 }}
+          >
+            <img className="imgss" src="/images/vs.png" alt="" />
+          </motion.div>
 
-                    <div className="versus">
-                        <img className='imgss' src="/images/vs.png" alt="" />
-                    </div>
+          <div className="others-conntain">
+            <motion.img
+              className="imgs"
+              src="/images/orther.png"
+              alt=""
+              animate={{ scale: [1, 1.1, 1], filter: ['drop-shadow(0 0 0px #ff0048)', 'drop-shadow(0 0 10px #ff0048)', 'drop-shadow(0 0 0px #ff0048)'] }}
+              transition={{ repeat: Infinity, duration: 2 }}
+            />
+            <span className="texts">OTHERS</span>
+          </div>
+        </motion.div>
 
-                    <div className="others-conntain">
-                        <img className='imgs' src="/images/orther.png" alt="" />
-                        <span className='texts'>OTHERS</span>
-                    </div>
-                </div>
+        {/* 🔹 Countdown Timer */}
+        <motion.div
+          className="dates"
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1.1 }}
+        >
+          <Timer targetDate={new Date(Date.now() + 40 * 24 * 60 * 60 * 1000)} />
+        </motion.div>
 
-                <div className="dates">
-                    <Timer targetDate={new Date(Date.now() + 40 * 24 * 60 * 60 * 1000)} />
-                    
-                </div>
+        {/* 🔹 CTA Button */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1, delay: 1.4 }}
+        >
+          <Link to="/community">
+            <button type="button" className="btn-chat btn btn-light">
+              Join The Chat
+            </button>
+          </Link>
+        </motion.div>
 
-                <Link to="/community">
-                <button type="button" className="btn-chat btn btn-light">Join The Chat</button>
-                </Link>
+        {/* 🔹 Bottom Updates Section */}
+        <motion.div
+          className="updates"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 1.8 }}
+        >
+          <div id="rowone" className="rowone">
+            <span id="ros">
+              <img className="dd" src="/images/codmbout.png" alt="" /> 
+              <p className="dd">Best of 5</p>
+            </span>
+            <span id="ros2">
+              <span className="dd">
+                <img src="/images/wlogo.png" alt="" /> &nbsp; XG
+              </span>
+              <span>
+                <Link to="/tournaments">
+                  <img src="/images/orther.png" alt="" />
+                </Link> &nbsp; NGC
+              </span>
+            </span>
+          </div>
 
-                <div className="updates">
-                    
-                        <div id='rowone' className="rowone">
-                        
-                             <span id='ros'><img className='dd'  src="/images/codmbout.png" alt="" /> <p className='dd'> Best of 5</p>
-                             
-                             {/* <img src="/images/wwlogo.png" alt="" /> */}
-                             </span>
-                             <span id='ros2'>
-                               <span className='dd'><img  src="/images/wlogo.png" alt="" /> &nbsp; XG</span> 
-                          <span> <Link to="/tournaments"><img src="/images/orther.png" alt="" /> </Link> &nbsp; NGC</span>   
-                             </span>
-                                </div>
-                        <div className="rowone">
-                             <span id='ros'> <p className='dd'> Free fire cup tournament 4v4</p>
-                             
-                             {/* <img src="/images/wwlogo.png" alt="" /> */}
-                             </span>
-                             <span id='ros2'>
-                               <span className='dd'><img  src="/images/wlogo.png" alt="" /> &nbsp; XG</span> 
-                          <span> <Link to="/tournaments"><img src="/images/hm1.png" alt="" /> </Link> NGC</span>   
-                             </span>    
-                        </div>
-                        <div className="rowone">
-                             <span id='ros'> <p className='dd'> Chess premiere - best of 3</p>
-                             
-                             {/* <img src="/images/wwlogo.png" alt="" /> */}
-                             </span>
-                             <span id='ros2'>
-                               <span> <Link to="/tournaments"><img  src="/images/wlogo.png" alt="" /> </Link> &nbsp; XG</span> 
-                          <span className='dd'><img src="/images/hm1.png" alt="" />  &nbsp; NGC</span>   
-                             </span>
-                        </div>
-                        <div className="rowone">
-                             <span id='rose'> <Link to="/tournaments"><img  src="/images/nmu.png" alt="" /> </Link>
-                              <p className='dd'> Join our community to know what's happening</p>
-                             
-                             {/* <img src="/images/wwlogo.png" alt="" /> */}
-                             </span>
-                             <span id='ros2'>
-                               {/* <span><img src="/images/wlogo.png" alt="" /> &nbsp; XG</span>  */}
-                          {/* <span><img src="/images/orther.png" alt="" />  &nbsp; NGC</span>    */}
-                             </span>
-                        </div>
-                </div>
-            </div>
+          <div className="rowone">
+            <span id="ros">
+              <p className="dd">Free Fire Cup Tournament 4v4</p>
+            </span>
+            <span id="ros2">
+              <span className="dd">
+                <img src="/images/wlogo.png" alt="" /> &nbsp; XG
+              </span>
+              <span>
+                <Link to="/tournaments">
+                  <img src="/images/hm1.png" alt="" />
+                </Link> &nbsp; NGC
+              </span>
+            </span>
+          </div>
 
-            <div className="sides">
-                <Side />
-            </div>
-        </div>
-    );
+          <div className="rowone">
+            <span id="ros">
+              <p className="dd">Chess Premiere - Best of 3</p>
+            </span>
+            <span id="ros2">
+              <span>
+                <Link to="/tournaments">
+                  <img src="/images/wlogo.png" alt="" />
+                </Link> &nbsp; XG
+              </span>
+              <span className="dd">
+                <img src="/images/hm1.png" alt="" /> &nbsp; NGC
+              </span>
+            </span>
+          </div>
+
+          <div className="rowone">
+            <span id="rose">
+              <Link to="/tournaments">
+                <img src="/images/nmu.png" alt="" />
+              </Link>
+              <p className="dd">Join our community to know what's happening</p>
+            </span>
+          </div>
+        </motion.div>
+      </div>
+
+      <div className="sides">
+        <Side />
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
